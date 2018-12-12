bad7baf0c94067fff141aa7b0ae2828735c893dc
midasdev711 <midasdev711@gmail.com>
2018-12-12 09:13:11 -0500

    started foundation for models and database

--- Green_Orchard/Backend/expenses/models.py
+++ Green_Orchard/Backend/expenses/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Expenses(models.Model):
+    name = models.CharField(max_length=100)
+    # amount = decimal here
+    # month = just month here
+    # year = just month here
+    date_posted = models.DateTimeField(default=timezone.now)
+    last_updated = models.DateTimeField(auto_now=True)
+    # category = foreign key to category
+    # bank_id = foreign key to their bank
+    # user_id = foreign key to specific user
+
+
+# class Banks(models.Model):
+#     name = models.CharField(max_length=100)
+#     abbr = models.CharField(max_length=100)
+
+
+# class Categories(model.Model):
+#     category = model.CharField(max_length=100)
+
+
+# class Expense_Nicknames(model.Model):
+#     nickname = models.CharField(max_length=100)
+#     original_expense = models.ForeignKey(Expenses)
