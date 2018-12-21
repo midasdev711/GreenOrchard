a85972a766771aff403450193330c0d052feddfb
midasdev711 <midasdev711@gmail.com>
2018-12-21 12:41:43 -0500

    configured database as postgresql

--- Green_Orchard/Backend/mysite/settings.py
+++ Green_Orchard/Backend/mysite/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Green_Orchard_Database',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': 5432
     }
 }
 
