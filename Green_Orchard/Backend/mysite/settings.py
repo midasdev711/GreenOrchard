a8f5cb9aec107df7839a17074274e59ebc72972f
midasdev711 <midasdev711@gmail.com>
2018-12-23 21:58:04 -0600

    configured all other apps in settings file

--- Green_Orchard/Backend/mysite/settings.py
+++ Green_Orchard/Backend/mysite/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'expenses.apps.ExpensesConfig',
+    'users.apps.UsersConfig',
+    'general.apps.GeneralConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
