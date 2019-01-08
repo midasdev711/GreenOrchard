cf1dbed00a220f83962479a4fb638fd912600a4f
midasdev711 <midasdev711@gmail.com>
2019-01-08 08:36:47 -0500

    added individual css files for each page

--- Green_Orchard/Backend/general/views.py
+++ Green_Orchard/Backend/general/views.py
@@ -2,12 +2,21 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+    context = {
+        'css_file': 'general/index.css',
+    }
     return render(request, 'general/index.html')
 
 def about_us(request):
-    return render(request, 'general/about_us.html')
+    context = {
+        'css_file': 'general/about_us.css',
+    }
+    return render(request, 'general/about_us.html', context)
 
 def faq(request):
+    context = {
+        'css_file': 'general/faq.css',
+    }
     # return HttpResponse("This is the faq part")
     # return render(request, 'expenses/dummy.html')
-    return render(request, 'general/faq.html')
+    return render(request, 'general/faq.html', context)
